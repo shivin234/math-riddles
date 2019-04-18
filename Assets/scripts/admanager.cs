b61a5f0bc35bacbfdb4fa9bc0af5205e8efd30ad
shivin sadana <shivinsadana123@gmail.com>
2019-04-18 18:47:57 +0530

    file

---
+++ Assets/scripts/admanager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleMobileAds.Api;
+
+public class admanager : MonoBehaviour {
+            
+    private string appID = "ca-app-pub-7783542980250893~8964070046";
+    private RewardBasedVideoAd rewardad1;
+    private string rewardad1ID = "ca-app-pub-7783542980250893/7667300241";
+
+
+
+    private void Start ()
+    {
+        MobileAds.Initialize(appID);
+        this.rewardad1 = RewardBasedVideoAd.Instance;
+
+
+        rewardad1.OnAdCompleted += Rewardad1_OnAdCompleted;
+
+    }
+
+    private void Rewardad1_OnAdCompleted(object sender, System.EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void loadad ()
+    {
+        AdRequest request = new AdRequest.Builder().Build();
+        this.rewardad1.LoadAd(request, rewardad1ID);
+        showad();
+
+    }
+
+    public void showad()
+    {
+        
+            rewardad1.Show();
+        
+    }
+
+}
---
+++ Assets/scripts/buttons.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class buttons : MonoBehaviour
+{
+    public Text inputfield;
+    public Text inputfield1;
+     int answer = 280;
+    public GameObject panel;
+
+    public void ButtonPressed()
+    {
+
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+
+        string buttonValue = EventSystem.current.currentSelectedGameObject.name;
+
+        
+        
+        if (buttonValue == "clear")
+        {
+            inputfield.text = "";
+        }
+        else if (buttonValue == "submit")
+        {
+            
+            if (Convert.ToInt32(inputfield.text) == answer)
+            {
+                inputfield1.text = "Correct";
+                SceneManager.LoadScene(2);
+            }
+            else
+            {
+                inputfield1.text = "InCorrect";
+            }
+            inputfield.text = "";
+        }
+        else if (inputfield.text == "")
+        {
+            inputfield.text = buttonValue;
+
+        }
+        else
+        {
+            inputfield.text += buttonValue;
+        }
+     
+    }
+    public void hide()
+    {
+        panel.gameObject.SetActive(false);
+       
+    }
+    
+}
